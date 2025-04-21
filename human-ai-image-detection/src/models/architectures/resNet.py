2a4f7ab2b02ee3f6a0e50cd5a76ee3a48d827cd0
IamShrijan <iamshrijan@gmail.com>
2025-04-21 14:12:24 -0400

    Adding resNet file

---
+++ human-ai-image-detection/src/models/architectures/resNet.py
@@ -0,0 +1,15 @@
+import torch
+
+import torch.nn as nn
+import torchvision.models as models
+
+class ModifiedResNet(nn.Module):
+    def __init__(self, num_classes=2):
+        super(ModifiedResNet, self).__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        num_features = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_features, num_classes)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        return x
